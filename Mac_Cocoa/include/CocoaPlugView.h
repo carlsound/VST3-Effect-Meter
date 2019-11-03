caf01817d8bbd41f1b165c4873e40a2a50c114ce
John <john.carlson@carlsound.net>
2019-11-03 12:54:46 -0600

    updated cocoaPlugView
    TODO: fix PlugView runtime error

--- Mac_Cocoa/include/CocoaPlugView.h
+++ Mac_Cocoa/include/CocoaPlugView.h
@@ -2,96 +2,120 @@
 //
 // Created by John Carlson on 10/13/19.
 //
-
-// public.sdk/source/common/pluginview.h
-//
 #pragma once
 //
 #include "base/source/fobject.h"
 //
-#include "pluginterfaces/gui/iplugview.h"
+//#include "pluginterfaces/gui/iplugview.h"
+#include "public.sdk/source/common/pluginview.h"
 //
 #include "meterView.h"
 #include "meterViewController.h"
 //
-class CocoaPlugView: public Steinberg::FObject,
-                     public Steinberg::IPlugView
+namespace Carlsound
 {
-public:
-    CocoaPlugView ();
-    //~CocoaPlugView ();
-    
-    //---from IPlugView-------
-    /** Is Platform UI Type supported
-	    \param type : IDString of \ref platformUIType */
-    Steinberg::tresult PLUGIN_API isPlatformTypeSupported (Steinberg::FIDString type) SMTG_OVERRIDE;
+    namespace Meter
+    {
+        class CocoaPlugView: public Steinberg::CPluginView //,
+                             //public Steinberg::IPlugView
+        {
+        public:
+            //---Interface------
+            //OBJ_METHODS (CocoaPlugView, Steinberg::CPluginView)
+            //DEFINE_INTERFACES
+            //    DEF_INTERFACE (Steinberg::IPlugView)
+            //END_DEFINE_INTERFACES (Steinberg::CPluginView)
+            //REFCOUNT_METHODS(Steinberg::CPluginView)
+            //
+            //
+            //
+            CocoaPlugView ();
+            CocoaPlugView (const Steinberg::ViewRect* rect);
+            ~CocoaPlugView ();
+            
+            //---from CPluginView-------
+            /** Returns its current frame rectangle. */
+            const Steinberg::ViewRect& getRect () const;
+            
+            /** Sets a new frame rectangle. */
+            void setRect (const Steinberg::ViewRect& r);
 
-    /** The parent window of the view has been created, the (platform) representation of the view
-        should now be created as well.
-        Note that the parent is owned by the caller and you are not allowed to alter it in any way
-        other than adding your own views.
-        Note that in this call the Plug-in could call a IPlugFrame::resizeView ()!
-        \param parent : platform handle of the parent window or view
-        \param type : \ref platformUIType which should be created */
-    Steinberg::tresult PLUGIN_API attached (void* parent, Steinberg::FIDString type) SMTG_OVERRIDE;
+            /** Checks if this view is attached to its parent view. */
+            bool isAttached () const;
 
-    /** The parent window of the view is about to be destroyed.
-        You have to remove all your own views from the parent window or view. */
-    Steinberg::tresult PLUGIN_API removed () SMTG_OVERRIDE;
+            /** Calls when this view will be attached to its parent view. */
+            void attachedToParent () SMTG_OVERRIDE;
 
-    /** Handling of mouse wheel. */
-    Steinberg::tresult PLUGIN_API onWheel (float distance) SMTG_OVERRIDE;
+            /** Calls when this view will be removed from its parent view. */
+            void removedFromParent () SMTG_OVERRIDE;
+            
+            //---from IPlugView-------
+            /** Is Platform UI Type supported
+                \param type : IDString of \ref platformUIType */
+            Steinberg::tresult PLUGIN_API isPlatformTypeSupported (Steinberg::FIDString type) SMTG_OVERRIDE;
 
-    /** Handling of keyboard events : Key Down.
-        \param key : unicode code of key
-        \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
-        \param modifiers : any combination of modifiers - see \ref KeyModifier in keycodes.h
-        \return kResultTrue if the key is handled, otherwise kResultFalse. \n
-                <b> Please note that kResultTrue must only be returned if the key has really been
-       handled. </b> Otherwise key command handling of the host might be blocked! */
-    Steinberg::tresult PLUGIN_API onKeyDown (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers) SMTG_OVERRIDE;
+            /** The parent window of the view has been created, the (platform) representation of the view
+                should now be created as well.
+                Note that the parent is owned by the caller and you are not allowed to alter it in any way
+                other than adding your own views.
+                Note that in this call the Plug-in could call a IPlugFrame::resizeView ()!
+                \param parent : platform handle of the parent window or view
+                \param type : \ref platformUIType which should be created */
+            Steinberg::tresult PLUGIN_API attached (void* parent, Steinberg::FIDString type) SMTG_OVERRIDE;
 
-    /** Handling of keyboard events : Key Up.
-        \param key : unicode code of key
-        \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
-        \param modifiers : any combination of KeyModifier - see \ref KeyModifier in keycodes.h
-        \return kResultTrue if the key is handled, otherwise return kResultFalse. */
-    Steinberg::tresult PLUGIN_API onKeyUp (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers) SMTG_OVERRIDE;
+            /** The parent window of the view is about to be destroyed.
+                You have to remove all your own views from the parent window or view. */
+            Steinberg::tresult PLUGIN_API removed () SMTG_OVERRIDE;
 
-    /** Returns the size of the platform representation of the view. */
-    Steinberg::tresult PLUGIN_API getSize (Steinberg::ViewRect* size) SMTG_OVERRIDE;
+            /** Handling of mouse wheel. */
+            Steinberg::tresult PLUGIN_API onWheel (float distance) SMTG_OVERRIDE;
 
-    /** Resizes the platform representation of the view to the given rect. Note that if the Plug-in
-     *	requests a resize (IPlugFrame::resizeView ()) onSize has to be called afterward. */
-    Steinberg::tresult PLUGIN_API onSize (Steinberg::ViewRect* newSize) SMTG_OVERRIDE;
+            /** Handling of keyboard events : Key Down.
+                \param key : unicode code of key
+                \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
+                \param modifiers : any combination of modifiers - see \ref KeyModifier in keycodes.h
+                \return kResultTrue if the key is handled, otherwise kResultFalse. \n
+                        <b> Please note that kResultTrue must only be returned if the key has really been
+               handled. </b> Otherwise key command handling of the host might be blocked! */
+            Steinberg::tresult PLUGIN_API onKeyDown (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers) SMTG_OVERRIDE;
 
-    /** Focus changed message. */
-    Steinberg::tresult PLUGIN_API onFocus (Steinberg::TBool state) SMTG_OVERRIDE;
+            /** Handling of keyboard events : Key Up.
+                \param key : unicode code of key
+                \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
+                \param modifiers : any combination of KeyModifier - see \ref KeyModifier in keycodes.h
+                \return kResultTrue if the key is handled, otherwise return kResultFalse. */
+            Steinberg::tresult PLUGIN_API onKeyUp (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers) SMTG_OVERRIDE;
 
-    /** Sets IPlugFrame object to allow the Plug-in to inform the host about resizing. */
-    Steinberg::tresult PLUGIN_API setFrame (Steinberg::IPlugFrame* frame) SMTG_OVERRIDE;
+            /** Returns the size of the platform representation of the view. */
+            Steinberg::tresult PLUGIN_API getSize (Steinberg::ViewRect* size) SMTG_OVERRIDE;
 
-    /** Is view sizable by user. */
-    Steinberg::tresult PLUGIN_API canResize () SMTG_OVERRIDE;
+            /** Resizes the platform representation of the view to the given rect. Note that if the Plug-in
+             *    requests a resize (IPlugFrame::resizeView ()) onSize has to be called afterward. */
+            Steinberg::tresult PLUGIN_API onSize (Steinberg::ViewRect* newSize) SMTG_OVERRIDE;
 
-    /** On live resize this is called to check if the view can be resized to the given rect, if not
-     *	adjust the rect to the allowed size. */
-    Steinberg::tresult PLUGIN_API checkSizeConstraint (Steinberg::ViewRect* rect) SMTG_OVERRIDE;
-    //
-    //---Interface------
-    OBJ_METHODS (CocoaPlugView, Steinberg::FObject)
-    DEFINE_INTERFACES
-        DEF_INTERFACE (Steinberg::IPlugView)
-    END_DEFINE_INTERFACES (Steinberg::FObject)
-    REFCOUNT_METHODS(Steinberg::FObject)
-    //
-protected:
-    //Steinberg::ViewRect m_rect;
-    //void* systemWindow;
-    //Steinberg::IPlugFrame* plugFrame;
-    //
-    NSView *m_parentView;
-    //NSWindow* m_parentWindow;
-    //meterView* m_view;
-    meterViewController *m_viewController;
-};
+            /** Focus changed message. */
+            Steinberg::tresult PLUGIN_API onFocus (Steinberg::TBool state) SMTG_OVERRIDE;
+
+            /** Sets IPlugFrame object to allow the Plug-in to inform the host about resizing. */
+            Steinberg::tresult PLUGIN_API setFrame (Steinberg::IPlugFrame* frame) SMTG_OVERRIDE;
+
+            /** Is view sizable by user. */
+            Steinberg::tresult PLUGIN_API canResize () SMTG_OVERRIDE;
+
+            /** On live resize this is called to check if the view can be resized to the given rect, if not
+             *    adjust the rect to the allowed size. */
+            Steinberg::tresult PLUGIN_API checkSizeConstraint (Steinberg::ViewRect* rect) SMTG_OVERRIDE;
+            //
+        protected:
+            Steinberg::ViewRect m_rect;
+            void* m_systemWindow;
+            Steinberg::IPlugFrame* m_plugFrame;
+            //
+            NSView *m_parentView;
+            //
+            //NSWindow* m_parentWindow;
+            //meterView* m_view;
+            meterViewController *m_viewController;
+        };
+    } // namespace Meter
+} // namespace Carlsound
--- Mac_Cocoa/source/CocoaPlugView.mm
+++ Mac_Cocoa/source/CocoaPlugView.mm
@@ -5,57 +5,70 @@
 #include "../include/cocoaPlugView.h"
 #include <string.h>
 
-CocoaPlugView::CocoaPlugView()
+namespace Carlsound
 {
-    //m_rect.left = 0;
-    //m_rect.right = 0;
-    //m_rect.top = 0;
-    //m_rect.bottom = 0;
-    //
-    //m_view = [[meterView alloc] initWithFrame: NSMakeRect(0, 0, 480, 272)];
-    //m_viewController = [[meterViewController alloc] initWithNibName: @"meterViewController" bundle: [NSBundle mainBundle /* bundleWithPath:@"net.carlsound.VST3-Effect-Meter" */]];
-}
-
-/** Is Platform UI Type supported
-	    \param type : IDString of \ref platformUIType */
-Steinberg::tresult CocoaPlugView::isPlatformTypeSupported (Steinberg::FIDString type)
-{
-    if(Steinberg::kPlatformTypeNSView == type)
+    namespace Meter
     {
-        return Steinberg::kResultTrue;
-    }
-    else
-    {
-        return Steinberg::kResultFalse;
-    }
-}
-
-/** The parent window of the view has been created, the (platform) representation of the view
-    should now be created as well.
-    Note that the parent is owned by the caller and you are not allowed to alter it in any way
-    other than adding your own views.
-    Note that in this call the Plug-in could call a IPlugFrame::resizeView ()!
-    \param parent : platform handle of the parent window or view
-    \param type : \ref platformUIType which should be created */
-Steinberg::tresult CocoaPlugView::attached (void* parent, Steinberg::FIDString type)
-{
-    if(NULL != type)
-    {
-        bool isTypeNSView = false;
-        for(int i = 0; i < (sizeof(type)-2); i++)
+        //------------------------------------------------------------------------
+        CocoaPlugView::CocoaPlugView():
+                        m_rect(0, 0, 0, 0),
+                        m_systemWindow(nullptr),
+                        m_plugFrame(nullptr)
         {
-            if(type[i] == Steinberg::kPlatformTypeNSView[i])
-            {
-                isTypeNSView = true;
-            }
-            else
-            {
-                isTypeNSView = false;
-            }
+        
         }
-        if(isTypeNSView)
+    
+        //------------------------------------------------------------------------
+        CocoaPlugView::CocoaPlugView(const Steinberg::ViewRect* rect):
+                        m_rect(0, 0, 0, 0),
+                        m_systemWindow(nullptr),
+                        m_plugFrame(nullptr)
         {
-            m_parentView = static_cast<NSView*>(parent);
+            if(rect)
+            {
+                m_rect = *rect;
+            }
+            //m_rect.left = 0;
+            //m_rect.right = 0;
+            //m_rect.top = 0;
+            //m_rect.bottom = 0;
+            //
+            //m_view = [[meterView alloc] initWithFrame: NSMakeRect(0, 0, 480, 272)];
+            //m_viewController = [[meterViewController alloc] initWithNibName: @"meterViewController" bundle: [NSBundle mainBundle /* bundleWithPath:@"net.carlsound.VST3-Effect-Meter" */]];
+        }
+        
+        //------------------------------------------------------------------------
+        CocoaPlugView::~CocoaPlugView()
+        {
+            
+        }
+        
+        //------------------------------------------------------------------------
+        /** Returns its current frame rectangle. */
+        const Steinberg::ViewRect& CocoaPlugView::getRect () const
+        {
+            return rect;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Sets a new frame rectangle. */
+        void CocoaPlugView::setRect (const Steinberg::ViewRect& r)
+        {
+            rect = r;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Checks if this view is attached to its parent view. */
+        bool CocoaPlugView::isAttached () const
+        {
+            return systemWindow != nullptr;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Calls when this view will be attached to its parent view. */
+        void CocoaPlugView::attachedToParent ()
+        {
+            m_parentView = static_cast<NSView*>(m_systemWindow);
             //m_parentWindow = static_cast<NSWindow*>(parent); //static_cast<NSView*>(parent);
             //
             //m_view = [[meterView alloc] initWithFrame: NSMakeRect(0, 0, 480, 272)];
@@ -72,87 +85,147 @@ Steinberg::tresult CocoaPlugView::attached (void* parent, Steinberg::FIDString t
             m_parentView.layer.backgroundColor = NSColor.yellowColor.CGColor;
             m_viewController.view.layer.backgroundColor = NSColor.blueColor.CGColor;
             //
+        }
+        
+        //------------------------------------------------------------------------
+        /** Calls when this view will be removed from its parent view. */
+        void CocoaPlugView::removedFromParent ()
+        {
+            //[m_view removeFromSuperview];
+            [m_viewController.view removeFromSuperview];
+            //[m_viewController dealloc];
+            //[m_parentView dealloc];
+            //[m_view dealloc];
+        }
+        
+        //------------------------------------------------------------------------
+        /** Is Platform UI Type supported
+                \param type : IDString of \ref platformUIType */
+        Steinberg::tresult CocoaPlugView::isPlatformTypeSupported (Steinberg::FIDString type)
+        {
+            if (strcmp (type, Steinberg::kPlatformTypeNSView) == 0)
+            {
+                return Steinberg::kResultTrue;
+            }
+            else
+            {
+                return Steinberg::kResultFalse;
+            }
+        }
+        
+        //------------------------------------------------------------------------
+        /** The parent window of the view has been created, the (platform) representation of the view
+            should now be created as well.
+            Note that the parent is owned by the caller and you are not allowed to alter it in any way
+            other than adding your own views.
+            Note that in this call the Plug-in could call a IPlugFrame::resizeView ()!
+            \param parent : platform handle of the parent window or view
+            \param type : \ref platformUIType which should be created */
+        Steinberg::tresult CocoaPlugView::attached (void* parent, Steinberg::FIDString type)
+        {
+            
+            if (strcmp (type, Steinberg::kPlatformTypeNSView) == 0)
+            {
+                m_systemWindow = parent;
+                //
+                attachedToParent ();
+                return Steinberg::kResultOk;
+            }
+            return Steinberg::kResultFalse;
+        }
+        
+        //------------------------------------------------------------------------
+        /** The parent window of the view is about to be destroyed.
+            You have to remove all your own views from the parent window or view. */
+        Steinberg::tresult CocoaPlugView::removed ()
+        {
+            m_systemWindow = nullptr;
+            //
+            attachedToParent ();
+            return Steinberg::kResultOk;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Handling of mouse wheel. */
+        Steinberg::tresult CocoaPlugView::onWheel (float distance)
+        {
             return Steinberg::kResultTrue;
         }
-    }
-    return Steinberg::kResultFalse;
-}
+        
+        //------------------------------------------------------------------------
+        /** Handling of keyboard events : Key Down.
+            \param key : unicode code of key
+            \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
+            \param modifiers : any combination of modifiers - see \ref KeyModifier in keycodes.h
+            \return kResultTrue if the key is handled, otherwise kResultFalse. \n
+                    <b> Please note that kResultTrue must only be returned if the key has really been
+           handled. </b> Otherwise key command handling of the host might be blocked! */
+        Steinberg::tresult CocoaPlugView::onKeyDown (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers)
+        {
+            return Steinberg::kResultTrue;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Handling of keyboard events : Key Up.
+            \param key : unicode code of key
+            \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
+            \param modifiers : any combination of KeyModifier - see \ref KeyModifier in keycodes.h
+            \return kResultTrue if the key is handled, otherwise return kResultFalse. */
+        Steinberg::tresult CocoaPlugView::onKeyUp (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers)
+        {
+            return Steinberg::kResultTrue;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Returns the size of the platform representation of the view. */
+        Steinberg::tresult CocoaPlugView::getSize (Steinberg::ViewRect* size)
+        {
+            if (size)
+            {
+                *size = m_rect;
+                return Steinberg::kResultTrue;
+            }
+            return Steinberg::kInvalidArgument;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Resizes the platform representation of the view to the given rect. Note that if the Plug-in
+         *    requests a resize (IPlugFrame::resizeView ()) onSize has to be called afterward. */
+        Steinberg::tresult CocoaPlugView::onSize (Steinberg::ViewRect* newSize)
+        {
+            if (newSize)
+                m_rect = *newSize;
+            return Steinberg::kResultTrue;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Focus changed message. */
+        Steinberg::tresult CocoaPlugView::onFocus (Steinberg::TBool state)
+        {
+            return Steinberg::kResultTrue;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Sets IPlugFrame object to allow the Plug-in to inform the host about resizing. */
+        Steinberg::tresult CocoaPlugView::setFrame (Steinberg::IPlugFrame* frame)
+        {
+            return Steinberg::kResultTrue;
+        }
+        
+        //------------------------------------------------------------------------
+        /** Is view sizable by user. */
+        Steinberg::tresult CocoaPlugView::canResize ()
+        {
+            return Steinberg::kResultTrue;
+        }
+        
+        //------------------------------------------------------------------------
+        /** On live resize this is called to check if the view can be resized to the given rect, if not
+         *    adjust the rect to the allowed size. */
+        Steinberg::tresult CocoaPlugView::checkSizeConstraint (Steinberg::ViewRect* rect)
+        {
+            return Steinberg::kResultTrue;
+        }
 
-/** The parent window of the view is about to be destroyed.
-    You have to remove all your own views from the parent window or view. */
-Steinberg::tresult CocoaPlugView::removed ()
-{
-    //[m_view removeFromSuperview];
-    [m_viewController.view removeFromSuperview];
-    //[m_viewController dealloc];
-    //[m_parentView dealloc];
-    //[m_view dealloc];
-    //
-    return Steinberg::kResultTrue;
-}
-
-/** Handling of mouse wheel. */
-Steinberg::tresult CocoaPlugView::onWheel (float distance)
-{
-    return Steinberg::kResultTrue;
-}
-
-/** Handling of keyboard events : Key Down.
-    \param key : unicode code of key
-    \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
-    \param modifiers : any combination of modifiers - see \ref KeyModifier in keycodes.h
-    \return kResultTrue if the key is handled, otherwise kResultFalse. \n
-            <b> Please note that kResultTrue must only be returned if the key has really been
-   handled. </b> Otherwise key command handling of the host might be blocked! */
-Steinberg::tresult CocoaPlugView::onKeyDown (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers)
-{
-    return Steinberg::kResultTrue;
-}
-
-/** Handling of keyboard events : Key Up.
-    \param key : unicode code of key
-    \param keyCode : virtual keycode for non ascii keys - see \ref VirtualKeyCodes in keycodes.h
-    \param modifiers : any combination of KeyModifier - see \ref KeyModifier in keycodes.h
-    \return kResultTrue if the key is handled, otherwise return kResultFalse. */
-Steinberg::tresult CocoaPlugView::onKeyUp (Steinberg::char16 key, Steinberg::int16 keyCode, Steinberg::int16 modifiers)
-{
-    return Steinberg::kResultTrue;
-}
-
-/** Returns the size of the platform representation of the view. */
-Steinberg::tresult CocoaPlugView::getSize (Steinberg::ViewRect* size)
-{
-    return Steinberg::kResultTrue;
-}
-
-/** Resizes the platform representation of the view to the given rect. Note that if the Plug-in
- *	requests a resize (IPlugFrame::resizeView ()) onSize has to be called afterward. */
-Steinberg::tresult CocoaPlugView::onSize (Steinberg::ViewRect* newSize)
-{
-    return Steinberg::kResultTrue;
-}
-
-/** Focus changed message. */
-Steinberg::tresult CocoaPlugView::onFocus (Steinberg::TBool state)
-{
-    return Steinberg::kResultTrue;
-}
-
-/** Sets IPlugFrame object to allow the Plug-in to inform the host about resizing. */
-Steinberg::tresult CocoaPlugView::setFrame (Steinberg::IPlugFrame* frame)
-{
-    return Steinberg::kResultTrue;
-}
-
-/** Is view sizable by user. */
-Steinberg::tresult CocoaPlugView::canResize ()
-{
-    return Steinberg::kResultTrue;
-}
-
-/** On live resize this is called to check if the view can be resized to the given rect, if not
- *	adjust the rect to the allowed size. */
-Steinberg::tresult CocoaPlugView::checkSizeConstraint (Steinberg::ViewRect* rect)
-{
-    return Steinberg::kResultTrue;
-}
+    } // namespace Meter
+} // namespace Carlsound
